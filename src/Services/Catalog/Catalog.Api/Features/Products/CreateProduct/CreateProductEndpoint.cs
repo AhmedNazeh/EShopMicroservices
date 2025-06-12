aa088095939b314e63e73eb1693483a587bc4a7b
Ahmed Nazeh <nazeh@iepm.sa>
2025-06-12 15:14:30 +0300

    Add MediatR support and create product functionality

---
+++ src/Services/Catalog/Catalog.Api/Features/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.Api.Features.Products.CreateProduct
+{
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Features.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, decimal Price, string Category, string ImageUrl)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Logic to create a product
+            var productId = Guid.NewGuid(); // Simulate product creation
+            return Task.FromResult(new CreateProductResult(productId));
+        }
+    }
+    
+}
