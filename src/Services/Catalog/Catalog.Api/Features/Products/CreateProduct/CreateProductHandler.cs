c883e964d2346f7702c73c6b79be6758c706f632
Ahmed Nazeh <nazeh@iepm.sa>
2025-06-18 23:37:44 +0300

    Add Marten integration for product management
    - Added Marten package reference in `Catalog.Api.csproj`.
    - Updated `CreateProductHandler` to use `IDocumentSession`.
    - Made `Handle` method asynchronous for database operations.
    - Updated global using directives to include necessary namespaces.
    - Configured Marten in `Program.cs` with database connection.
    - Added PostgreSQL connection string in `appsettings.json`.

--- src/Services/Catalog/Catalog.Api/Features/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Features/Products/CreateProduct/CreateProductHandler.cs
@@ -1,22 +1,25 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.Api.Models;
+﻿
 
 namespace Catalog.Api.Features.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, decimal Price, List<string> Category, string ImageFile)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandHandler (IDocumentSession session): ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
 
             var product = request.Adapt<Product>();
            
 
             // Logic to create a product
-            var productId = Guid.NewGuid(); // Simulate product creation
-            return Task.FromResult(new CreateProductResult(productId));
+
+            session.Store(product);
+           await  session.SaveChangesAsync(cancellationToken);
+
+            var productId =product.Id; // Simulate product creation
+            return new CreateProductResult(productId);
         }
     }
     
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddMediatR(config => {
   config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("database")!);
+
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
