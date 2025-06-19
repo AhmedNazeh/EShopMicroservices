305b2c90966ebf5b625ae8d319ff55a96f582085
Ahmed Nazeh <nazeh@iepm.sa>
2025-06-19 15:13:36 +0300

    Refactor CQRS and Docker setup for Catalog.Api
    - Changed `IQueryHandler` interface to public with constraints.
    - Added `<DockerComposeProjectPath>` to `Catalog.Api.csproj`.
    - Updated `Dockerfile` to include `BuildingBlocks.csproj` and expose port `8081`.
    - Modified connection string in `appsettings.json`.
    - Updated solution file to include Docker Compose project.
    - Created `docker-compose.dcproj`, `docker-compose.override.yml`, and `docker-compose.yml` for database service configuration.
    - Added `launchSettings.json` for Docker Compose settings.
    - Refactored `Dockerfile.original` for new build stages.
    - Implemented `GetProductsEndpoint` and `GetProductsHandler` for product retrieval.

---
+++ src/Services/Catalog/Catalog.Api/Features/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Catalog.Api.Features.Products.GetProducts
+{
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var query = new GetProductsQuery();
+                var products = await sender.Send(query);
+                return Results.Ok(products);
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Features/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.Api.Features.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<IEnumerable<Product>>;
+
+    public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+     
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            return products;
+        }
+    }
+   
+    
+}
